b9e0ecedc6a992b0ed8cc4f8c0746460a97ead50
Ray Zana <rayzana280@gmail.com>
2022-12-16 12:29:42 -0500

    commit tree 6713444b0dea25afda24958556dff37a8bf13149

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+const headquarter = 42
+function distanceFromHqInBlocks(someValue){
+    if(headquarter>=someValue){
+        return headquarter-someValue
+    }else{
+        return someValue - headquarter
+    }
+
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start>=destination){
+        return (start-destination)*264
+    }else{
+        return (destination-start)*264
+    }
+}
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination);
+    if(feet<=400){
+        return 0;
+    }else if(feet > 400 && feet < 2000){
+        return (feet - 400) * .02;
+    }else if(feet > 2000 && feet < 2500){
+        return 25;
+    }else if(feet>=2500){
+        return 'cannot travel that far'
+    }
+    
+    //add if statement 400 is free
+    //add else if for charinging 2 cent between 400 - 2000 feet 
+    //add else if charging flat fare over 2000-2500 ft
+    // over 2500 'cannot travel that far' else
+}
+
+console.log(calculatesFarePrice(34, 32));
